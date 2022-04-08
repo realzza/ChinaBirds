84398ff1d37c5530bca2ff9f2276d4cb3fd2f7c5
ziang <zz188@duke.edu>
2022-04-08 03:58:18 +0000

    achieved average sampling. weaken longtail problem

--- indexGen_multi.py
+++ indexGen_multi.py
@@ -1,9 +1,11 @@
 import os
 import sox
 import json
+import math
 import random
 import argparse
 from tqdm import tqdm
+from sox.file_info import duration
 from multiprocessing import Process
 
 def parse_args():
@@ -13,13 +15,19 @@ def parse_args():
     parser.add_argument('--outDir', type=str, default=None, help="path to the output dir")
     parser.add_argument('--process', type=int, default=20, help="number of process running")
     parser.add_argument('--test-size', type=float, default=0.15, help="ratio of test set from all")
-    parser.add_argument('--threshold', type=int, default=1000, help="threshold of data of each class")
+    parser.add_argument('--threshold', type=int, default=1000, help="number of samples from each wav file")
     parser.add_argument('--class2count', type=str, default="class2count.json", help="class2count json file")
     parser.add_argument('--isTrain', action='store_true', help="parse the train dataset")
     return parser.parse_args()
 
+def get_floor(x, n):
+    return math.floor(x * 10**n)/10**n
 
-def gen(pid, nproc, all_records, outDir, isTrain):
+def gen(pid, args, all_records, class2nsample):
+    nproc = args.process
+    outDir = args.outDir
+    isTrain = args.isTrain
+    
     isFirst = True
     
     portion = len(all_records) // nproc
@@ -41,10 +49,12 @@ def gen(pid, nproc, all_records, outDir, isTrain):
             isFirst = False
         
         rec_len = sox.file_info.duration(rec)
-        if rec_len > 5:
-            max_start = int(rec_len) - 4
-            tagged_utt2wav += [rec.split('/')[-1].split('.')[0] + '_%d '%i + rec.replace("data-","feats-").replace('.wav','.h5') + ' %d'%i for i in range(max_start)]
-            tagged_utt2label += [rec.split('/')[-1].split('.')[0] + '_%d '%i + rec.split('/')[-2] for i in range(max_start)]
+        bird_species = rec.split('/')[-2]
+        bird_samples = class2nsample[bird_species]
+#         if rec_len > 5:
+        nstarts = [get_floor(random.uniform(0, rec_len-5),2) for i in range(bird_samples)]
+        tagged_utt2wav += [rec.split('/')[-1].split('.')[0] + '_%d '%i + rec.replace("aligned","feats").replace('.wav','.h5') + ' %.2f'%fn for i,fn in enumerate(nstarts)]
+        tagged_utt2label += [rec.split('/')[-1].split('.')[0] + '_%d '%i + bird_species for i in range(len(nstarts))]
     
     if isTrain:
         sec = 'train'
@@ -66,14 +76,20 @@ if __name__ == '__main__':
     dataDir = args.dataDir
     allClips = []
     with open(args.class2count,'r') as f:
-        all_birds = list(json.load(f).keys())
+        all_birds_class2count = json.load(f)
+    all_birds = list(all_birds_class2count.keys())
+    all_birds_class2sample = {k:int(args.threshold//all_birds_class2count[k])+1 for k in all_birds_class2count}
         
+    # init output directories
+    outDir = args.outDir.rstrip('/') + '/'
+    if not os.path.isdir(outDir):
+        os.mkdir(outDir)
     
     # train/test set
     if args.isTrain:
-        with open('%strain_utt2wav'%args.outDir,'w') as f:
+        with open('%strain_utt2wav'%outDir,'w') as f:
             f.write('')
-        with open('%strain_utt2label'%args.outDir,'w') as f:
+        with open('%strain_utt2label'%outDir,'w') as f:
             f.write('')
         for bird in all_birds:
             tmpClips = os.listdir(dataDir+bird)
@@ -83,9 +99,9 @@ if __name__ == '__main__':
             tmpClips = tmpClips[int(args.test_size*len(tmpClips)):]
             allClips += [dataDir + bird + '/' + x for x in tmpClips if x.endswith('mp3') or x.endswith('wav')]
     else:
-        with open('%sdevel_utt2wav'%args.outDir,'w') as f:
+        with open('%sdevel_utt2wav'%outDir,'w') as f:
             f.write('')
-        with open('%sdevel_utt2label'%args.outDir,'w') as f:
+        with open('%sdevel_utt2label'%outDir,'w') as f:
             f.write('')
         for bird in all_birds:
             tmpClips = os.listdir(dataDir+bird)
@@ -96,16 +112,11 @@ if __name__ == '__main__':
             allClips += [dataDir + bird + '/' + x for x in tmpClips if x.endswith('mp3') or x.endswith('wav')]
         
     
-    # init output directories
-    outDir = args.outDir.strip('/') + '/'
-    if not os.path.isdir(outDir):
-        os.mkdir(outDir)
-        
     # multiprocessing transformation
     worker_count = args.process
     worker_pool = []
     for i in range(worker_count):
-        p = Process(target=gen, args=(i, worker_count, allClips, outDir, args.isTrain))
+        p = Process(target=gen, args=(i, args, allClips, all_birds_class2sample))
         p.start()
         worker_pool.append(p)
     for p in worker_pool:
